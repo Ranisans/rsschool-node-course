da152d52d428ea7119c63f72c2f3771004036036
Rani <ranisans@yahoo.com>
2020-03-27 13:12:19 +0300

    feat: add a check if the input file is exist

--- task1/fileLogic.js
+++ task1/fileLogic.js
@@ -1,6 +1,18 @@
 const fs = require('fs');
+const { promisify } = require('util');
 
-exports.createReadStream = filename => {
+const { FILE_ERROR } = require('./constant');
+const exit = require('./exit');
+
+const access = promisify(fs.access);
+
+exports.createReadStream = async filename => {
+  try {
+    await access(filename, fs.constants.F_OK);
+  } catch (error) {
+    console.log(FILE_ERROR);
+    exit();
+  }
   return fs.createReadStream(filename);
 };
 
