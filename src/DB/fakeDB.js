3dde95beb4cb934059d9c237c81b71b4418c321e
Rani <ranisans@yahoo.com>
2020-03-31 18:03:56 +0300

    feat: add fake DB gnerator generate when the server started

---
+++ src/DB/fakeDB.js
@@ -0,0 +1,28 @@
+const fs = require('fs').promises;
+
+const { users, boards, columns, tasks } = require('./tables');
+
+const readData = async (filename, table) => {
+  return fs
+    .readFile(filename)
+    .then(data => JSON.parse(data))
+    .then(result => {
+      result.forEach(userData => {
+        table.push(userData);
+      });
+    })
+    .catch(error => {
+      console.log('readData -> error', error);
+      const exit = process.exit;
+      exit(1);
+    });
+};
+
+const generateDB = async () => {
+  await readData('./src/DB/data/Users.json', users);
+  await readData('./src/DB/data/Boards.json', boards);
+  await readData('./src/DB/data/Columns.json', columns);
+  await readData('./src/DB/data/Tasks.json', tasks);
+};
+
+module.exports = generateDB;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 const { PORT } = require('./common/config');
 const app = require('./app');
+const generateDB = require('./DB/fakeDB');
+
+generateDB();
 
 app.listen(PORT, () =>
   console.log(`App is running on http://localhost:${PORT}`)
