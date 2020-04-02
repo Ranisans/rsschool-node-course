2a95d5c7932e71b3811ada47b32dd6f38ddcba68
Rani <ranisans@yahoo.com>
2020-04-02 13:41:57 +0300

    refact: update user router

--- src/routes/users/router.js
+++ src/routes/users/router.js
@@ -9,40 +9,40 @@ const {
 } = require('./logic');
 const { OK, ERROR, SERVER_ERROR } = require('../../statusCodes');
 
-router.get('/', async (req, res) => {
-  res.json(getAllUser());
-});
+router
+  .route('/:id')
+  .get(async (req, res) => {
+    const { id } = req.params;
+    res.json(getUserById(id));
+  })
+  .delete(async (req, res) => {
+    const { id } = req.params;
+    const result = deleteUserById(id);
+    res.sendStatus(result ? OK : ERROR);
+  });
 
-router.get('/:id', async (req, res) => {
-  const { id } = req.params;
-  res.json(getUserById(id));
-});
-
-router.post('/', async (req, res) => {
-  const { name, login, password } = req.body;
-  if (name === undefined || login === undefined || password === undefined) {
-    res.sendStatus(ERROR);
-    return;
-  }
-  const result = addNewUser({ name, login, password });
-  res.sendStatus(result ? OK : SERVER_ERROR);
-});
-
-router.put('/', async (req, res) => {
-  const { id, name, login, password } = req.body;
-  console.log('id', id);
-  if (id === undefined) {
-    res.sendStatus(ERROR);
-    return;
-  }
-  const result = updateUserById({ id, name, login, password });
-  res.sendStatus(result ? OK : SERVER_ERROR);
-});
-
-router.delete('/:id', async (req, res) => {
-  const { id } = req.params;
-  const result = deleteUserById(id);
-  res.sendStatus(result ? OK : ERROR);
-});
+router
+  .route('/')
+  .get(async (req, res) => {
+    res.json(getAllUser());
+  })
+  .post(async (req, res) => {
+    const { name, login, password } = req.body;
+    if (name === undefined || login === undefined || password === undefined) {
+      res.sendStatus(ERROR);
+      return;
+    }
+    const result = addNewUser({ name, login, password });
+    res.sendStatus(result ? OK : SERVER_ERROR);
+  })
+  .put(async (req, res) => {
+    const { id, name, login, password } = req.body;
+    if (id === undefined) {
+      res.sendStatus(ERROR);
+      return;
+    }
+    const result = updateUserById({ id, name, login, password });
+    res.sendStatus(result ? OK : SERVER_ERROR);
+  });
 
 module.exports = router;
