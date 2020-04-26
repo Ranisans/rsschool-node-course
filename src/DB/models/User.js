9c36f1ed30264f1ec89482510df15a997725affa
Rani <ranisans@yahoo.com>
2020-04-26 19:23:28 +0300

    feat: add User schema validation for hashing password add compare Password function

--- src/DB/models/User.js
+++ src/DB/models/User.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const uuid = require('uuid');
+const bcrypt = require('bcrypt');
 
-const userSchema = mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
     name: String,
     login: String,
@@ -14,10 +15,33 @@ const userSchema = mongoose.Schema(
   { versionKey: false }
 );
 
+userSchema.pre('validate', function callback(next) {
+  if (!this.isModified('password')) return next();
+
+  bcrypt.genSalt((err, salt) => {
+    if (err) return next(err);
+
+    bcrypt.hash(this.password, salt, (error, hash) => {
+      if (error) return next(error);
+
+      this.password = hash;
+      next();
+    });
+  });
+});
+
 userSchema.static('toResponse', user => {
   const { _id, name, login } = user;
   return { id: _id, name, login };
 });
+
+userSchema.methods.comparePassword = async function comparePassword(password) {
+  const match = await bcrypt.compare(password, this.password);
+  if (match) {
+    return { userId: this._id, login: this.login };
+  }
+  return false;
+};
 
 const User = mongoose.model('User', userSchema);
 
